8ba5499edae53693c029e67bb777c5f0f6a7c17f
adnannextdoor <136168925+adnannextdoor@users.noreply.github.com>
2023-12-18 13:17:50 +0000

    Add files via upload
    Custom GPT that utilises the ChatGPT-3.5-turbo model to create a chatbot that can be customised to the user's preference.
    
    Signed-off-by: adnannextdoor <136168925+adnannextdoor@users.noreply.github.com>

---
+++ gpt.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "user", "content": system_msg})
+
+print("Adnan GPT is ready to use!")
+while input != "quit()":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=messages)
+    reply = response.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
