0c3f550072b4e056bc59c38b5862b98da65c50f3
adnannextdoor <136168925+adnannextdoor@users.noreply.github.com>
2023-12-27 15:47:44 +0000

    Update blockchain_v1.py
    Adding timestamp to record block creation more accurately and to increase security
    
    Including transactions element to make it more implementable and useful
    
    Signed-off-by: adnannextdoor <136168925+adnannextdoor@users.noreply.github.com>

--- blockchain_v1.py
+++ blockchain_v1.py
@@ -1,14 +1,16 @@
 import hashlib
+import time
 
 class Block:
-    def __init__(self, data, prev_hash):
-        self.data = data
+    def __init__(self, transactions, prev_hash):
+        self.transactions = transactions
         self.prev_hash = prev_hash
+        self.timestamp = time.time()
         self.hash = self.calc_hash()
 
     def calc_hash(self):
         sha = hashlib.sha256()
-        sha.update(self.data.encode('utf-8'))
+        sha.update((str(self.transactions) + str(self.prev_hash) + str(self.timestamp)).encode('utf-8'))
         return sha.hexdigest()
 
 class Blockchain:
@@ -16,22 +18,22 @@ class Blockchain:
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        return Block("Genesis Block", "0")
+        return Block([], "0")
 
-    def add_block(self, data):
+    def add_block(self, transactions):
         prev_block = self.chain[-1]
-        new_block = Block(data, prev_block)
+        new_block = Block(transactions, prev_block.hash)
         self.chain.append(new_block)
 
 blockchain = Blockchain()
-
-blockchain.add_block('First Block')
-blockchain.add_block('Second Block')
-blockchain.add_block('Third Block')
+blockchain.add_block(transactions=[{'sender' : 'Adnan', 'recipient' : 'Julie', 'Amount' : 5}])
+blockchain.add_block(transactions=[{'sender' : 'Adnan', 'recipient' : 'Navin', 'Amount' : 4}])
+blockchain.add_block(transactions=[{'sender' : 'Adnan', 'recipient' : 'Mohamed', 'Amount' : 7}])
 
 print("Blockchain:")
 for block in blockchain.chain:
-    print("Data", block.data)
+    print("Data", block.transactions)
     print("Previous Hash:", block.prev_hash)
     print("Hash:", block.hash)
-
+    print("Timestamp: ", block.timestamp)
+    print()
